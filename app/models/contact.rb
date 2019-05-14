4a3dc81a33eda6146791b948f4203ac4c9a0e43b
agarcian031 <algarcian@outlook.com>
2019-05-14 12:31:27 -0600

    Add instance method

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -11,4 +11,15 @@ class Contact < ApplicationRecord
   def self.by_first_name
     order(:first_name)
   end 
+
+  #Instance Method - self being the instance the method is called on
+  def full_name 
+    "#{self.first_name} #{self.last_name}"
+  end 
 end
+
+<% @contacts.each do |contact| %>
+  <h1> <%= contact.first_name %> <%= contact.last_name %> </h1> 
+  <h1> <%= contact.full_name </h1> 
+
+  <% end %> 
