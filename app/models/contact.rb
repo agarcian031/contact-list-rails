38a39b32917e1c0d24461bb3de0f534952dfa943
agarcian031 <algarcian@outlook.com>
2019-05-14 12:23:26 -0600

    Add validations, associations, class methods

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,13 @@ class Contact < ApplicationRecord
   # Associations 
   has_many :notes, dependent: :destroy  
   has_one :address, dependent: :destroy 
+
+  #Validations 
+  validates :email, :first_name, :last_name,  presence: true 
+  validates :email, uniqueness: true 
+
+  #Class Method 
+  def self.by_first_name
+    order(:first_name)
+  end 
 end
